eaf5f60f7bbc5d24de60341f532e967e7025e68a
viumiuu <viumiu4@gmail.com>
2025-03-23 13:19:36 +0200

    game.py

--- game.py
+++ game.py
@@ -1,56 +1,106 @@
-from pygame import*
-from assets import*
+from pygame import *
+from assets import *
 from random import randint
 
+# фонова музика
 mixer.init()
 mixer.music.load(GAME_MUSIC)
 mixer.music.play(-1)
 fire_sound = mixer.Sound(FIRE_SOUND)
+damage_sound = mixer.Sound(DAMAGE_SOUND)
 
-img_back = GAME_BG_IMG
-img_hero = ROCKET_IMG
-img_enemy = ENEMY_IMG
 
+# нам потрібні такі картинки:
+img_back = GAME_BG_IMG  # фон гри
+img_hero = ROCKET_IMG  # герой
+img_enemy = ENEMY_IMG # ворог
+
+font.init()
+font2= font.Font(None, 36)
+
+font1 = font.Font(None, 80)
+win = font1.render("YOU WIN", True, (255, 255, 255))
+lose = font1.render("YOU LOSE", True, (100, 0, 0))
+
+score = 0
+lost = 0
+
+# клас-батько для інших спрайтів
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+    # конструктор класу
+    def __init__(self, player_image, player_x, player_y, 
+                 size_x, size_y, player_speed):
+        # викликаємо конструктор класу (Sprite):
         sprite.Sprite.__init__(self)
- 
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        
+        # кожен спрайт повинен зберігати властивість image - зображення
+        self.image = transform.scale(
+            image.load(player_image), (size_x, size_y))
         self.speed = player_speed
+        # кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-        def reset(self):
-            window.blit(self.image, (self.rect.x, self.rect.y))   
 
+    # метод, що малює героя на вікні
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+# клас головного гравця
 class Player(GameSprite):
     def update(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
+        if keys[K_a] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+        if keys[K_d] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
     def fire(self):
         pass
+
+class Enemy (GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+            lost = lost + 1
+
 
 win_width = 700
 win_height = 500
 display.set_caption("Shooter")
 window = display.set_mode((win_width, win_height))
-background = transform.scale(image. load(img_back), (win_width, win_height))
+background = transform.scale(image.load(img_back), 
+                             (win_width, win_height))
 
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
-finish = False
 
-run = True
+monsters = sprite.Group()
+for i in range(1, 6):
+    monster = Enemy(img_enemy, randint(80, win_width - 80), - 40, 80, 50, randint(1, 5))
+    monsters.add(monster)
+
+finish = False
+run = True  # прапорець скидається кнопкою закриття вікна
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
     if not finish:
         window.blit(background, (0, 0))
+        text = font2.render("Рахунок: " + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
+
+        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+
         ship.update()
+
+        monsters.update()
+
         ship.reset()
+
+        monsters.draw(window)
+
         display.update()
     time.delay(50)
